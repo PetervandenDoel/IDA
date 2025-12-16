1e5d0df168164f182b0276877e7a5ea79a42ab87
Peter van den Doel <PetervandenDoel24@gmail.com>
2025-12-16 10:04:03 -0800

    changes to init and controller for manager integration

--- motors/hal/__init__.py
+++ motors/hal/__init__.py
@@ -2,7 +2,7 @@ from motors.hal.motors_hal import MotorHAL
 from motors.hal.stage_factory import register_driver, create_driver
 from motors.hal.emotor_factory import register_driver, create_driver
 
-from motors.optical import ida_controller, modern_stage
+from motors.optical import ida_controller, modern_stage, scylla_controller
 from motors.elec import BSC203_controller
 
 __all__ = ['MotorHAL', 'register_driver', 'create_driver']
